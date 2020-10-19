208681d26e5f4ee529959ab981de70c9f22c2ecf
netotz <neto.otz@hotmail.com>
2020-10-18 22:25:20 -0500

    Add unit tests for default template
    These example tests should be removed after actual tests are added

---
+++ Elecritic.Tests/WeatherForecastTests.cs
@@ -0,0 +1,25 @@
+﻿using Elecritic.Models;
+
+using Xunit;
+
+namespace Elecritic.UnitTests {
+
+    public class WeatherForecastTests {
+
+        [Theory]
+        [InlineData(0, 32)]
+        [InlineData(-40, -40)]
+        public void GetFahrenheit_FromCelsius_ReturnFahrenheitConversion(int input, int expected) {
+            // arrange
+            var weatherForecast = new WeatherForecast {
+                TemperatureCelsius = input
+            };
+
+            // act
+            var getFahrenheit = weatherForecast.TemperatureFahrenheit;
+
+            // assert
+            Assert.Equal(expected, getFahrenheit);
+        }
+    }
+}
