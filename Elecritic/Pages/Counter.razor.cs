1337404b5ce7843d16e7bebe91336ce8da43bf3f
netotz <neto.otz@hotmail.com>
2020-10-18 20:12:40 -0500

    Add Blazor WebAssembly default template

---
+++ Elecritic/Pages/Counter.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Elecritic.Pages {
+    public partial class Counter {
+
+        private int CurrentCount { get; set; } = 0;
+
+        private void IncrementCount() {
+            CurrentCount++;
+        }
+    }
+}
---
+++ Elecritic/Pages/FetchData.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+using Elecritic.Models;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Elecritic.Pages {
+
+    public partial class FetchData {
+
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        private WeatherForecast[] Forecasts { get; set; }
+
+        protected override async Task OnInitializedAsync() {
+            Forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+    }
+}
