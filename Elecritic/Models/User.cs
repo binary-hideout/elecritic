94b40e32478defdf38eb633d183d4babc73ecfd0
netotz <neto.otz@hotmail.com>
2020-10-31 14:44:35 -0600

    Create User class
    See #3

---
+++ Elecritic/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Elecritic.Models {
+
+    public class User {
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Represents a user's score according to how reliable his/her reviews and ratings are.
+        /// </summary>
+        public int Reliability { get; set; }
+    }
+}
