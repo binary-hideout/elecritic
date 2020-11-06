81dbb89cd50c077c295bc8572386d851690c1105
Diegobr21 <jdiegobrana@gmail.com>
2020-11-06 13:07:04 -0600

    Created ProductService.cs and Added field Imagepath to Product.cs

---
+++ Elecritic/Models/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elecritic.Models {
+    public class ProductService {
+
+        private static readonly string[] ImagesPath = new[] {
+            "carousel-images/huawei.jpg", "carousel-images/iphone.png", "carousel-images/lg.jpg",
+            "carousel-images/motorola.jpg", "carousel-images/samsung.jpg"
+        };
+
+        public Task<Product[]> GetForecastAsync(DateTime startDate) {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Product {
+                Id = 1,
+                Category = "1",
+                Company = "Apple",
+                Name = "Celular",
+                Description = "Buen estado",
+                Imagepath = ImagesPath[rng.Next(ImagesPath.Length)],
+                ReleaseDate = startDate.AddDays(index),
+            }).ToArray());
+        }
+    }
+}
