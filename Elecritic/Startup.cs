6de0453dfa260816d3636bff8d2fa1889ff30d1c
Diegobr21 <jdiegobrana@gmail.com>
2020-11-06 13:08:30 -0600

    Added Singleton of ProductService

--- Elecritic/Startup.cs
+++ Elecritic/Startup.cs
@@ -20,6 +20,7 @@ namespace Elecritic {
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
