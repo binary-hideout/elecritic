e058d89fa98c00cd2aeaa9a7d6ea4ac75b171fd0
Milton Carlos Rodríguez Polina <miltonrdz25@hotmail.com>
2020-11-07 17:04:27 -0600

    Size Cards and margin, +Images

--- Elecritic/Models/ProductService.cs
+++ Elecritic/Models/ProductService.cs
@@ -8,12 +8,13 @@ namespace Elecritic.Models {
 
         private static readonly string[] ImagesPath = new[] {
             "carousel-images/huawei.jpg", "carousel-images/iphone.png", "carousel-images/lg.jpg",
-            "carousel-images/motorola.jpg", "carousel-images/samsung.jpg"
+            "carousel-images/motorola.jpg", "carousel-images/samsung.jpg", "carousel-images/xiaomi.jpg",
+            "carousel-images/alienware.jpg","carousel-images/laptop_hp.png", "carousel-images/lenovo.jpg"
         };
 
         public Task<Product[]> GetRandomProductsAsync(DateTime startDate) {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Product {
+            return Task.FromResult(Enumerable.Range(1, 20).Select(index => new Product {
                 Id = 1,
                 Category = "1",
                 Company = "Apple",
