29d7171880887788af043cf8c2f170248a471f82
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-11-06 18:50:19 -0600

    Add review model class

---
+++ Elecritic/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elecritic.Models {
+    public class Review {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Test { get; set; }
+        public int Rating { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
