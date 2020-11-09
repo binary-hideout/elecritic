5bc47b14fd43ad0bd7930bbca17d77bff4430da3
Diegobr21 <jdiegobrana@gmail.com>
2020-11-09 12:15:38 -0600

    Created ProductPage partial class

---
+++ Elecritic/Pages/ProductPage.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elecritic.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Elecritic.Pages {
+    public partial class ProductPage {
+
+        [Parameter]
+        public string ProductId { get; set; }
+    }
+}
