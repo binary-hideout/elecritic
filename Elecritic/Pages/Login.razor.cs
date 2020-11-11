721f906807a4a0c149aa13480fd5432818508ec6
Robertudo <sayamosfero@gmail.com>
2020-11-11 16:24:59 -0600

    feat<Login,Register,Host,CSS,imports> add the login and  register interface, their respective models and the radzen library

---
+++ Elecritic/Pages/Login.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elecritic.Pages {
+    public partial class LoginRequest {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
---
+++ Elecritic/Pages/Register.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elecritic.Pages {
+    public partial class RegisterRequest {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ConfirmPassword { get; set; }
+    }
+}
