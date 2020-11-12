b4802412ce4f7b9907ad75fda88b783d5e684173
netotz <neto.otz@hotmail.com>
2020-11-11 22:38:45 -0600

    Add Category model class

---
+++ Elecritic/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿namespace Elecritic.Models {
+    /// <summary>
+    /// Category of a <see cref="Product"/>.
+    ///  To get the actual name, e.g. <c>"Cellphone"</c>, access <see cref="Name"/>
+    /// </summary>
+    public class Category {
+
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Actual name of this category.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get the string representation of the category.
+        /// </summary>
+        /// <returns><see cref="Name"/></returns>
+        public override string ToString() {
+            return Name;
+        }
+    }
+}
