8697edf7475f73b867a793c0ee9438472224374d
netotz <neto.otz@hotmail.com>
2020-11-11 21:00:05 -0600

    Remove initial template classes

--- Elecritic/Models/WeatherForecast.cs
+++ Elecritic/Models/WeatherForecast.cs
@@ -2,6 +2,10 @@
 
 namespace Elecritic.Models {
 
+    /// <summary>
+    /// Sample class. It's only purpose is to demonstrate the use of a sample unit test (see Elecritic.UnitTests).
+    /// If there is no test using this, remove the class.
+    /// </summary>
     public class WeatherForecast {
 
         public DateTime Date { get; set; }
