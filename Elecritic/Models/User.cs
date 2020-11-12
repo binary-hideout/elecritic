9e5fcc33ceee6e7fb10e43e6fe4635205ca2d27b
netotz <neto.otz@hotmail.com>
2020-11-12 00:12:35 -0600

    Modify User model class

--- Elecritic/Models/User.cs
+++ Elecritic/Models/User.cs
@@ -1,5 +1,10 @@
-﻿namespace Elecritic.Models {
+﻿using System.Collections.Generic;
 
+namespace Elecritic.Models {
+
+    /// <summary>
+    /// Class that represents a user account.
+    /// </summary>
     public class User {
 
         public int Id { get; set; }
@@ -13,8 +18,23 @@
         public string LastName { get; set; }
 
         /// <summary>
+        /// Hashed password. Useful when trying a login.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
         /// Represents a user's score according to how reliable his/her reviews and ratings are.
         /// </summary>
         public int Reliability { get; set; }
+
+        /// <summary>
+        /// Reviews made by the user.
+        /// </summary>
+        public virtual List<Review> Reviews { get; set; }
+
+        /// <summary>
+        /// Opinions of reviews from other users.
+        /// </summary>
+        public virtual List<Opinion> Opinions { get; set; }
     }
 }
