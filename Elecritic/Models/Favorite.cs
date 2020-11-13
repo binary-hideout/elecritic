d5e85c5f679f38d7688b1910ad32a482622a3bf1
netotz <neto.otz@hotmail.com>
2020-11-13 14:42:48 -0600

    Add Favorite model class

---
+++ Elecritic/Models/Favorite.cs
@@ -0,0 +1,20 @@
+﻿namespace Elecritic.Models {
+
+    /// <summary>
+    /// Class that represents the record of a product marked as favorite by a user.
+    /// </summary>
+    public class Favorite {
+
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User who marked the product as favorite.
+        /// </summary>
+        public virtual User User { get; set; }
+
+        /// <summary>
+        /// Product marked as favorite.
+        /// </summary>
+        public virtual Product Product { get; set; }
+    }
+}
