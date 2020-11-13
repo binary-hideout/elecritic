bbbca20078101ff986c054614a5c37cc62756b1b
netotz <neto.otz@hotmail.com>
2020-11-13 14:49:39 -0600

    Modify Favorite class docstring

--- Elecritic/Models/Favorite.cs
+++ Elecritic/Models/Favorite.cs
@@ -1,19 +1,19 @@
 ﻿namespace Elecritic.Models {
 
     /// <summary>
-    /// Class that represents the record of a product marked as favorite by a user.
+    /// Class that represents the record of a <see cref="Models.Product"/> marked as favorite by a <see cref="Models.User"/>.
     /// </summary>
     public class Favorite {
 
         public int Id { get; set; }
 
         /// <summary>
-        /// User who marked the product as favorite.
+        /// User who marked the <see cref="Product"/>.
         /// </summary>
         public virtual User User { get; set; }
 
         /// <summary>
-        /// Product marked as favorite.
+        /// Product marked as favorite by <see cref="User"/>.
         /// </summary>
         public virtual Product Product { get; set; }
     }
