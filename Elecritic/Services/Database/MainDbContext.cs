8e42941e5e3b4f75ff9590c2f368b6c512c19f30
netotz <neto.otz@hotmail.com>
2020-11-12 19:08:01 -0600

    Add main database context class

---
+++ Elecritic/Services/Database/MainDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace Elecritic.Services.Database {
+
+    /// <summary>
+    /// Application database context. Builds every necessary model class to a MySQL table.
+    /// This context does not contain any call to the database, and it is not injected as a service (is not usable).
+    /// The usable contexts inherit from this class.
+    /// Its purpose is to use the Code First feature of Entity Framework.
+    /// </summary>
+    public class MainDbContext : DbContext {
+
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options) { }
+
+        protected MainDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
