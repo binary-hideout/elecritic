413d0938226a248d0905d7e769d0250fdaac6bd6
netotz <neto.otz@hotmail.com>
2020-11-13 20:23:12 -0600

    Create empty Hasher helper class

---
+++ Elecritic/Helpers/Hasher.cs
@@ -0,0 +1,9 @@
+﻿namespace Elecritic.Helpers {
+
+    /// <summary>
+    /// Helper class that provides a static method to hash a password.
+    /// </summary>
+    public static class Hasher {
+
+    }
+}
