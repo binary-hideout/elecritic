47439c7e9f91bc282d9de0820acfff4d07298125
netotz <neto.otz@hotmail.com>
2020-11-13 21:15:23 -0600

    Add method to hash a password

--- Elecritic/Helpers/Hasher.cs
+++ Elecritic/Helpers/Hasher.cs
@@ -1,9 +1,35 @@
-﻿namespace Elecritic.Helpers {
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Elecritic.Helpers {
 
     /// <summary>
     /// Helper class that provides a static method to hash a password.
     /// </summary>
     public static class Hasher {
 
+        /// <summary>
+        /// Gets the bytes from <paramref name="inputString"/> and applies SHA256 to get their hash.
+        /// </summary>
+        /// <returns>Hash of <paramref name="inputString"/> bytes.</returns>
+        private static byte[] GetHash(string inputString) {
+            var bytes = Encoding.UTF8.GetBytes(inputString);
+
+            using var sha256 = SHA256.Create();
+            return sha256.ComputeHash(bytes);
+        }
+
+        /// <summary>
+        /// Applies SHA256 algorithm to hash a raw password, and returns the hash.
+        /// </summary>
+        /// <param name="password">A raw password to hash.</param>
+        /// <returns>The hashed password.</returns>
+        public static string GetHashedPassword(string password) {
+            var stringBuilder = new StringBuilder();
+            foreach (var hashedByte in GetHash(password)) {
+                stringBuilder.Append(hashedByte.ToString("x2"));
+            }
+            return stringBuilder.ToString();
+        }
     }
 }
