2a8d12afa838437e728d8a60107871740483b4e0
Daniel Contreras <contrerasgz.daniel@gmail.com>
2020-11-17 17:10:43 -0600

    UI to upload CSV files

--- Elecritic/Pages/UploadFiles.razor.cs
+++ Elecritic/Pages/UploadFiles.razor.cs
@@ -5,27 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Elecritic.Pages {
-    public partial class LoginAdmin {
 
-        private LoginRequest ModelAdmin { get; set; } = new LoginRequest();
 
-        public async Task LoginUserAdmin() {
-
-        }
-
-        void GoToRegister() {
-            navigationManager.NavigateTo("/upload-files");
-        }
-
-        public class LoginRequest {
-            [Required]
-            [StringLength(50)]
-            [EmailAddress]
-            public string Email { get; set; }
-
-            [Required]
-            [StringLength(50)]
-            public string Password { get; set; }
-        }
-    }
 }
