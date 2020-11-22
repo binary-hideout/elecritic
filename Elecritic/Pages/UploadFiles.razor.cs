845f62eda79e1db8bc078c41dac4c7530e4193f9
Diegobr21 <jdiegobrana@gmail.com>
2020-11-22 13:05:03 -0600

    Created a FileUploadModel, still not working

--- Elecritic/Pages/UploadFiles.razor.cs
+++ Elecritic/Pages/UploadFiles.razor.cs
@@ -16,13 +16,25 @@ namespace Elecritic.Pages {
 
     public partial class UploadFiles {
 
+        public string filePath { get; set; }
+        public string dummy { get; set; }
+        public FileUploadModel fileUploadModel { get; set; }
+        private void Upload() {
+            dummy = filePath;
 
+            //Check if the file is valid
+            //Store the filepath in a string
+            //Call ReadFile(filepath)
+            //ReadFile(filePath);
+        }
+
+        
         /// <summary>
         /// This method should receive the filepath of the excel or csv file where the products are at
         /// and then it converts them into a list of Products to be then saved in the DB
         /// </summary>
         /// <param name="productTable"></param>
-        /// <returns></returns>
+        /// <returns> A list with the products recognized from the table </returns>
         public List<Product> ReadFile(string productTable) {
 
             List<Product> productsInFile = new List<Product>();
@@ -56,6 +68,23 @@ namespace Elecritic.Pages {
 
             return productsInFile;
 
+        }
+    }
+
+    /// <summary>
+    /// Model created to valid the file input 
+    /// </summary>
+    public class FileUploadModel {
+
+        [Required]
+        public Stream File { get; set; }
+        public FileUploadModel() {
+
+        }
+
+        public FileUploadModel(Stream file) {
+            File = file;
+            
         }
     }
 
