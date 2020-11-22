0abbcb53036c2f56ea4f02ff64fe675a6c7d2c07
netotz <neto.otz@hotmail.com>
2020-11-22 17:13:47 -0600

    Use BlazorInputFile package

--- Elecritic/Pages/UploadFiles.razor.cs
+++ Elecritic/Pages/UploadFiles.razor.cs
@@ -2,15 +2,15 @@
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Components.Forms;
 using Elecritic.Models;
 using Elecritic.Services;
 
 using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using Radzen;
-using OfficeOpenXml;
 using System.IO;
+using BlazorInputFile;
 
 namespace Elecritic.Pages {
 
@@ -28,47 +28,52 @@ namespace Elecritic.Pages {
             //ReadFile(filePath);
         }
 
-        
+        private IFileListEntry FileEntry { get; set; }
+
+        private void HandleFile(IFileListEntry[] files) {
+            FileEntry = files.FirstOrDefault();
+        }
+
         /// <summary>
         /// This method should receive the filepath of the excel or csv file where the products are at
         /// and then it converts them into a list of Products to be then saved in the DB
         /// </summary>
         /// <param name="productTable"></param>
         /// <returns> A list with the products recognized from the table </returns>
-        public List<Product> ReadFile(string productTable) {
+        //public List<Product> ReadFile(string productTable) {
 
-            List<Product> productsInFile = new List<Product>();
+        //    List<Product> productsInFile = new List<Product>();
 
-            string FilePath = productTable;
-            System.IO.FileInfo existingFile = new System.IO.FileInfo(FilePath);
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            using (ExcelPackage package = new ExcelPackage(existingFile))
-            {
-                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
-                int colCount = worksheet.Dimension.End.Column;
-                int rowCount = worksheet.Dimension.End.Row;
+        //    string FilePath = productTable;
+        //    System.IO.FileInfo existingFile = new System.IO.FileInfo(FilePath);
+        //    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        //    using (ExcelPackage package = new ExcelPackage(existingFile))
+        //    {
+        //        ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+        //        int colCount = worksheet.Dimension.End.Column;
+        //        int rowCount = worksheet.Dimension.End.Row;
 
-                for (int row = 0; row < rowCount; row++) {
+        //        for (int row = 0; row < rowCount; row++) {
 
-                    Product product = new Product();
+        //            Product product = new Product();
 
-                    for (int col = 0; col < colCount; col++) {
-                        if (col == 1) product.ImagePath = worksheet.Cells[row, col].Value.ToString();
-                        else if (col == 2) {
-                            product.Name = worksheet.Cells[row, col].Value.ToString();
-                        }else if(col == 3) {
-                            product.Description = worksheet.Cells[row, col].Value.ToString();
-                        }
+        //            for (int col = 0; col < colCount; col++) {
+        //                if (col == 1) product.ImagePath = worksheet.Cells[row, col].Value.ToString();
+        //                else if (col == 2) {
+        //                    product.Name = worksheet.Cells[row, col].Value.ToString();
+        //                }else if(col == 3) {
+        //                    product.Description = worksheet.Cells[row, col].Value.ToString();
+        //                }
                             
-                    }
-                    productsInFile.Add(product);
-                }
-            }
+        //            }
+        //            productsInFile.Add(product);
+        //        }
+        //    }
 
 
-            return productsInFile;
+        //    return productsInFile;
 
-        }
+        //}
     }
 
     /// <summary>
