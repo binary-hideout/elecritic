b442f1e6573feaaec7cd012535802991792aa222
netotz <neto.otz@hotmail.com>
2020-11-26 17:13:56 -0600

    Add login functionality

--- Elecritic/Pages/Login.razor.cs
+++ Elecritic/Pages/Login.razor.cs
@@ -1,6 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
+using Elecritic.Database;
+using Elecritic.Helpers;
+
 using Microsoft.AspNetCore.Components;
 
 namespace Elecritic.Pages {
@@ -10,14 +13,38 @@ namespace Elecritic.Pages {
         [Inject]
         private NavigationManager NavigationManager { get; set; }
 
+        [Inject]
+        private UserContext UserContext { get; set; }
+
         private UserDto Model { get; set; } = new UserDto();
 
+        private string ResultMessage { get; set; } = "";
+
         /// <summary>
-        /// Method not implemented yet.
+        /// Queries the database for the password of a user whose email matches the one provided.
+        /// If both passwords match, the rest of the user's data is retrieved.
         /// </summary>
-        /// <returns></returns>
         public async Task LogInUser() {
-            await Task.CompletedTask;
+            ResultMessage = "Iniciando sesión...";
+
+            string hashedPassword = Hasher.GetHashedPassword(Model.Password);
+            string dbPassword = await UserContext.GetHashedPasswordAsync(Model.Email);
+
+            if (string.IsNullOrEmpty(dbPassword)) {
+                ResultMessage =
+                    $"Parece que no existe ninguna cuenta con el correo '{Model.Email}'. " +
+                    "Intenta crear una nueva ;)";
+                return;
+            }
+
+            if (hashedPassword == dbPassword) {
+                var user = await UserContext.GetUserAsync(Model.Email);
+                ResultMessage = "¡Sesión iniciada! :D";
+                NavigationManager.NavigateTo("/");
+            }
+            else {
+                ResultMessage = "Contraseña incorrecta.";
+            }
         }
 
         void GoToSignup() {
