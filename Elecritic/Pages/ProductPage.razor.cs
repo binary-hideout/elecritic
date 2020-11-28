0d9db692aebef61d639abcf31f13c291764f3bb7
netotz <neto.otz@hotmail.com>
2020-11-27 20:25:50 -0600

    Add functionality to add or delete a product from user's favorites

--- Elecritic/Pages/ProductPage.razor.cs
+++ Elecritic/Pages/ProductPage.razor.cs
@@ -24,15 +24,67 @@ namespace Elecritic.Pages {
         [Inject]
         private UserService UserService { get; set; }
 
-        private string PublicationMessage { get; set; } = "";
-
         private Product Product { get; set; }
 
         private ReviewDto ReviewModel { get; set; } = new ReviewDto();
 
+        private Favorite Favorite { get; set; }
+
+        /// <summary>
+        /// Determines if <see cref="Product"/> is marked as favorite by <see cref="UserService.LoggedUser"/>.
+        /// </summary>
+        private bool IsFavorite { get; set; }
+
+        private string PublicationMessage { get; set; } = "";
+
+        /// <summary>
+        /// Message that explains if a database call with <see cref="Favorite"/> succeeded or not.
+        /// If it's not <see cref="string.Empty"/> then the buttons will be disabled.
+        /// </summary>
+        private string FavoriteChangedMessage { get; set; } = "";
+
         protected override async Task OnInitializedAsync() {
             Product = await ProductContext.GetProductAsync(ProductId);
             Product.Reviews = await ProductContext.GetProductReviewsAsync(Product);
+
+            int userId = UserService.LoggedUser.Id;
+            // if there's a user logged in
+            if (userId != 0) {
+                Favorite = await ProductContext.GetFavoriteAsync(userId, ProductId);
+                // favorite is null if the record doesn't exist,
+                // meaning that this product wouldn't be marked as favorite by logged user
+                IsFavorite = !(Favorite is null);
+            }
+        }
+
+        /// <summary>
+        /// Marks <see cref="Product"/> as favorite of <see cref="UserService.LoggedUser"/>.
+        /// </summary>
+        private async Task AddToFavoritesAsync() {
+            Favorite = new Favorite {
+                User = UserService.LoggedUser,
+                Product = Product
+            };
+            bool newFavoriteSucceeded = await ProductContext.InsertFavoriteAsync(Favorite);
+            if (newFavoriteSucceeded) {
+                FavoriteChangedMessage = $"¡Ahora te gusta {Product.Name}!";
+            }
+            else {
+                FavoriteChangedMessage = "Lo sentimos, ocurrió un error al marcar como favorito :(";
+            }
+        }
+
+        /// <summary>
+        /// Removes <see cref="Product"/> from favorites of <see cref="UserService.LoggedUser"/>.
+        /// </summary>
+        private async Task RemoveFromFavoritesAsync() {
+            bool removedFavoriteSucceeded = await ProductContext.DeleteFavoriteAsync(Favorite);
+            if (removedFavoriteSucceeded) {
+                FavoriteChangedMessage = $"Ya no te gusta {Product.Name}.";
+            }
+            else {
+                FavoriteChangedMessage = "Lo sentimos, ocurrió un error al quitar de tus favoritos :(";
+            }
         }
 
         /// <summary>
