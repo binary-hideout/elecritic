19579a46497b782e9b591c2d5715d30f5a00db8b
Diegobr21 <jdiegobrana@gmail.com>
2020-12-01 12:32:18 -0600

    Added partial class NavMenu2

---
+++ Elecritic/Shared/NavMenu2.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elecritic.Shared {
+    public partial class NavMenu2 {
+        private bool collapseNavMenu = true;
+
+        private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu() {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
