04ba8e29f6cea10aededf3453f1635e0a3474e85
Diegobr21 <jdiegobrana@gmail.com>
2020-12-01 12:28:41 -0600

    Check for MainLayout if User is Logged

--- Elecritic/Shared/MainLayout.razor.cs
+++ Elecritic/Shared/MainLayout.razor.cs
@@ -1,9 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
+using Elecritic.Database;
+using Elecritic.Models;
+using Elecritic.Services;
+
+using Microsoft.AspNetCore.Components;
+
 namespace Elecritic.Shared {
-    public class MainLayout {
+    public partial class MainLayout {
+
+        [Inject]
+        private UserService UserService { get; set; }
+
+        private bool UserLogged = false;
+        protected override void OnInitialized() {
+            var user = UserService.LoggedUser.Id;
+            if (user != 0) {
+                UserLogged = true;
+            }
+        }
     }
 }
