e6681088304cde446b3fc8de4cbd119d51809221
Diegobr21 <jdiegobrana@gmail.com>
2020-12-01 19:53:38 -0600

    Created ProductCard component with Product parameter

---
+++ Elecritic/Shared/ProductCard.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+using Elecritic.Database;
+using Elecritic.Models;
+using Elecritic.Services;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Elecritic.Shared {
+    public partial class ProductCard {
+        [Parameter]
+        public Product product { get; set; }
+    }
+}
