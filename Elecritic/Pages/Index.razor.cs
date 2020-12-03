47768be9cee1845b2f58ca982d6577cc6c2c1a01
netotz <neto.otz@hotmail.com>
2020-12-03 00:58:38 -0600

    Get recommended products in index page

--- Elecritic/Pages/Index.razor.cs
+++ Elecritic/Pages/Index.razor.cs
@@ -2,7 +2,10 @@
 using System.Threading.Tasks;
 
 using Elecritic.Database;
+using Elecritic.Helpers;
 using Elecritic.Models;
+using Elecritic.Services;
+
 using Microsoft.AspNetCore.Components;
 
 namespace Elecritic.Pages {
@@ -10,6 +13,12 @@ namespace Elecritic.Pages {
     public partial class Index {
         [Inject]
         private IndexContext IndexContext { get; set; }
+
+        [Inject]
+        private MyFavoritesContext MyFavoritesContext { get; set; }
+
+        [Inject]
+        private UserService UserService { get; set; }
 
         /// <summary>
         /// Customized recommended products for logged in user.
@@ -29,6 +38,14 @@ namespace Elecritic.Pages {
         protected override async Task OnInitializedAsync() {
             PopularProducts = await IndexContext.GetPopularProductsAsync();
             FavoriteProducts = await IndexContext.GetFavoriteProductsAsync();
+
+            int userId = UserService.LoggedUser.Id;
+            if (userId != 0) {
+                var userFavoriteProducts = await MyFavoritesContext.GetFavoriteProductsAsync(userId);
+                var products = await IndexContext.Get3StarsProductsAsync();
+
+                RecommendedProducts = FuzzyLogic.GetRecommended(userFavoriteProducts, products, 10);
+            }
         }
     }
 }
