0eedc699d03e57aa7fb5d496e7b55892b1054f34
netotz <neto.otz@hotmail.com>
2020-12-03 22:58:56 -0600

    Filter recommended products

--- Elecritic/Helpers/FuzzyLogic.cs
+++ Elecritic/Helpers/FuzzyLogic.cs
@@ -9,6 +9,16 @@ namespace Elecritic.Helpers {
     public static class FuzzyLogic {
 
         public static List<Product> GetRecommended(List<Product> userFavorites, List<Product> products, int totalRecommendedItems) {
+            // filter  IDs of products others than user favorites
+            var noFavoritesIds = products
+                .Select(p => p.Id)
+                .Except(userFavorites
+                    .Select(p => p.Id));
+            // select products from above IDs
+            products = products
+                .Where(p => noFavoritesIds.Contains(p.Id))
+                .ToList();
+
             var recommendedProducts = new List<Product>();
             for (int i = 1; i < 4; i++) {
                 int number = userFavorites.Count(p => p.Category.Id == i);
@@ -23,9 +33,17 @@ namespace Elecritic.Helpers {
             }
 
             int lacks = 10 - recommendedProducts.Count;
-            var remaining = products.Except(recommendedProducts).ToList();
+            if (lacks > 0) {
+                // filter IDs of not yet recommended products
+                var noRecommendedIds = products
+                    .Select(p => p.Id)
+                    .Except(recommendedProducts
+                        .Select(p => p.Id));
+                // select remaining products from above IDs
+                var remaining = products
+                    .Where(p => noRecommendedIds.Contains(p.Id))
+                    .ToList();
 
-            if (remaining.Count > 0) {
                 var random = new Random();
                 int[] randomIndexes = Enumerable
                     .Range(0, lacks)
