bf6552aef0de75f1db99616557a07230386ecace
netotz <neto.otz@hotmail.com>
2020-12-08 19:37:53 -0600

    Refactor MainLayout

--- Elecritic/Shared/MainLayout.razor.cs
+++ Elecritic/Shared/MainLayout.razor.cs
@@ -1,47 +1,29 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-
-using Elecritic.Database;
-using Elecritic.Models;
-using Elecritic.Services;
+﻿using Elecritic.Services;
 
 using Microsoft.AspNetCore.Components;
 
 namespace Elecritic.Shared {
-    public partial class MainLayout {
+    public partial class MainLayout : LayoutComponentBase {
 
-        
         [Inject]
         private UserService UserService { get; set; }
 
-        public bool UserLogged { get; set; }
+        public bool UserIsLogged { get; set; }
 
         /// <summary>
         /// Method calls for UserService to know if the userId is different from 0 as that means user is logged
         /// </summary>
         public void CheckLogged() {
-            int userId = UserService.LoggedUser.Id;
-            if(userId != 0) {
-                UserLogged = true;
-                this.StateHasChanged();
-            }
-            else {
-                UserLogged = false;
-                this.StateHasChanged();
-            }
+            UserIsLogged = UserService.LoggedUser.Id != 0;
+            StateHasChanged();
         }
        
-
         /// <summary>
         /// Checks if the user has changed its state (logged or not) each time the page is refreshed
         /// </summary>
         /// <param name="firstRender"> This bool as the name implies is true when the page is first rendered</param>
         protected override void OnAfterRender(bool firstRender) {
             CheckLogged();
-            
         }
-        
-
     }
 }
