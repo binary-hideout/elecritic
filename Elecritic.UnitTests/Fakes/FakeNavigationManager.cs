3603e468bd830d851ab1610e950e817a44971188
netotz <neto.otz@hotmail.com>
2020-12-08 22:22:26 -0600

    Add class to fake NavigationManager service

---
+++ Elecritic.UnitTests/Fakes/FakeNavigationManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Elecritic.UnitTests.Fakes {
+
+    /// <summary>
+    /// Fake object to replace <see cref="NavigationManager"/> service.
+    /// </summary>
+    public class FakeNavigationManager : NavigationManager {
+
+        /// <summary>
+        /// Initializes the service with dummy links.
+        /// </summary>
+        public FakeNavigationManager() {
+            Initialize("https://fake.link/", "https://fake.link/test/");
+        }
+
+        /// <summary>
+        /// Do not switch pages as it's a fake object.
+        /// </summary>
+        protected override void NavigateToCore(string uri, bool forceLoad) {
+            Uri = uri;
+        }
+    }
+}
