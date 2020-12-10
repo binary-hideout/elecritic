2cfe0140e7b8f6e98e9f6117035d998a12b1f170
netotz <neto.otz@hotmail.com>
2020-12-09 20:53:02 -0600

    Modify method to return a default instance if categoryId isn't found

--- Elecritic/Database/CategoryProductsContext.cs
+++ Elecritic/Database/CategoryProductsContext.cs
@@ -18,14 +18,22 @@ namespace Elecritic.Database {
 
         /// <summary>
         /// Queries the database for a specified category and populates its list of products.
+        /// If it doesn't exist, a default instance will be returned, with Id = 0.
         /// </summary>
         /// <param name="categoryId">Id of the category.</param>
         /// <param name="batchSize">Number of products to retrieve.</param>
         /// <param name="skipSize">Number of products to skip before retrieving <paramref name="batchSize"/> products.</param>
-        /// <returns><see cref="Category"/> with <see cref="Product"/>s of size <paramref name="batchSize"/>.</returns>
+        /// <returns><see cref="Category"/> including <see cref="Category.Products"/> with count <paramref name="batchSize"/> if exists,
+        /// else a default instance with <see cref="Category.Id"/> equal to <c>0</c>.
+        /// </returns>
         public async Task<Category> GetCategoryWithProductsAsync(int categoryId, int batchSize, int skipSize = 0) {
             var category = await CategoriesTable
-                .SingleAsync(c => c.Id == categoryId);
+                .SingleOrDefaultAsync(c => c.Id == categoryId);
+            // if there's no category that matches specified Id
+            if (category is null) {
+                // return a default category, Id = 0
+                return new Category();
+            }
 
             category.Products = await Entry(category)
                 .Collection(c => c.Products)
