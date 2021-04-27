9cda47ad6b543a313f2b72b60c7135d251a0e3fc
netotz <neto.otz@hotmail.com>
2021-04-27 16:27:07 -0500

    Add page to show all categories from database

---
+++ Elecritic/Features/Categories/Pages/CategoriesPage.razor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Elecritic.Features.Categories.Queries;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Elecritic.Features.Categories.Pages {
+    public partial class CategoriesPage {
+        [Inject]
+        private IMediator Mediator { get; set; }
+        private List<Lists.CategoryDto> Categories { get; set; }
+        private bool IsLoading { get; set; }
+
+        public CategoriesPage() {
+            Categories = new List<Lists.CategoryDto>();
+            IsLoading = false;
+        }
+
+        protected override async Task OnInitializedAsync() {
+            IsLoading = true;
+
+            Categories = (await Mediator.Send(new Lists.Query()))
+                .Categories;
+
+            IsLoading = false;
+        }
+    }
+}
