04825d86a3a1bfc9e6a0d5eeb9ef93790ad7127c
netotz <neto.otz@hotmail.com>
2021-04-28 11:08:49 -0500

    Add command to add favorite in ProductDetails feature

---
+++ Elecritic/Features/ProductDetails/Commands/AddFavorite.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Elecritic.Database;
+using Elecritic.Models;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Elecritic.Features.ProductDetails.Commands {
+    public class AddFavorite {
+        public class Command : IRequest<bool> {
+            public Favorite Favorite { get; set; }
+        }
+
+        public class CommandHandler : IRequestHandler<Command, bool> {
+            private readonly IDbContextFactory<ElecriticContext> _factory;
+            private readonly ILogger<CommandHandler> _logger;
+
+            public CommandHandler(IDbContextFactory<ElecriticContext> factory, ILogger<CommandHandler> logger) {
+                _factory = factory;
+                _logger = logger;
+            }
+
+            public async Task<bool> Handle(Command request, CancellationToken cancellationToken) {
+                _logger.LogInformation($"Handling command {request}: {{@request}}", request);
+
+                try {
+                    using var dbContext = _factory.CreateDbContext();
+
+                    dbContext.Entry(request.Favorite.User).State = EntityState.Unchanged;
+                    dbContext.Entry(request.Favorite.Product).State = EntityState.Unchanged;
+
+                    await dbContext.Favorites.AddAsync(request.Favorite);
+                    await dbContext.SaveChangesAsync();
+
+                    return true;
+                }
+                catch (DbUpdateException) {
+                    return false;
+                }
+            }
+        }
+    }
+}
