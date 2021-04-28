7282c09e694d47c5dc8ca7bcdce3ad0baae0ff6c
netotz <neto.otz@hotmail.com>
2021-04-28 13:11:22 -0500

    Add signup form model

---
+++ Elecritic/Features/Signup/Models/SignupForm.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Elecritic.Features.Signup.Models {
+    public class SignupForm {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Username { get; set; }
+
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [StringLength(25)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
+        [Compare(nameof(Password), ErrorMessage = "La contraseña debe coincidir.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
