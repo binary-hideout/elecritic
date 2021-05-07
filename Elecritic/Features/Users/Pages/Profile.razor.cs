b91fc559ea67a31a7290067b1ad5fdb5d13b164a
MILTON CARLOS RODRIGUEZ POLINA <milton.rodriguezpo@uanl.edu.mx>
2021-05-06 22:37:28 -0500

    Profile User

---
+++ Elecritic/Features/Users/Pages/Profile.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Elecritic.Features.Products.Queries;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Components;
+using Elecritic.Models;
+
+namespace Elecritic.Features.Users.Pages {
+    public partial class Profile {
+      /*  [Inject]
+        private User User { get; set; }
+
+        public Profile() {
+        }
+
+        protected override async Task OnInitializedAsync() { 
+
+            User = new User();
+
+        }*/
+    }
+}
