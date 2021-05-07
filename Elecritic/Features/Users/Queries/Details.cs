ac9918f41353519f3e0a06a373a97ab5a0d41be2
netotz <neto.otz@hotmail.com>
2021-05-07 12:06:22 -0500

    Add query to get user details

---
+++ Elecritic/Features/Users/Queries/Details.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Elecritic.Database;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Elecritic.Features.Users.Queries {
+    public class Details {
+        public record UserDto {
+            public string Username { get; init; }
+            public string Email { get; init; }
+            public string FullName { get; init; }
+            public string RoleName { get; init; }
+        }
+
+        public record Query : IRequest<UserDto> {
+            public int UserId { get; init; }
+        }
+
+        public class Handler : IRequestHandler<Query, UserDto> {
+            private readonly IDbContextFactory<ElecriticContext> _factory;
+            private readonly ILogger<Handler> _logger;
+
+            public Handler(IDbContextFactory<ElecriticContext> factory, ILogger<Handler> logger) {
+                _factory = factory;
+                _logger = logger;
+            }
+
+            public async Task<UserDto> Handle(Query request, CancellationToken cancellationToken) {
+                _logger.LogInformation($"Handling query {nameof(Details)}: {{@r}}", request);
+
+                using var dbContext = _factory.CreateDbContext();
+
+                return await dbContext.Users
+                    .Where(u => u.Id == request.UserId)
+                    .Select(u => new UserDto {
+                        Username = u.Username,
+                        Email = u.Email,
+                        FullName = $"{u.FirstName} {u.LastName}",
+                        RoleName = u.Role.Name
+                    })
+                    .SingleOrDefaultAsync();
+            }
+        }
+    }
+}
